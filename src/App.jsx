ed15bc75f3fc796b205082f23d7485043910e147
Lorena Rincon <lorec.rincon@gmail.com>
2023-08-11 13:24:15 -0300

    Project setup and home page responsive

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import "./assets/styles/home.css";
+import "./assets/styles/prueba.css";
+import Home from "./assets/components/Home";
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
